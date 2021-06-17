4bb44707530e9c85c9ebeacefa07dbf7030910f4
Fabricio Dourado <65264206+fvieir@users.noreply.github.com>
2021-06-16 22:28:29 -0300

    Criando template, css e ajustando controller home

--- App/Controllers/HomeController.php
+++ App/Controllers/HomeController.php
@@ -6,7 +6,16 @@ class HomeController
 {
     public function index()
     {
-        echo "index da home";
+        $url = 'http://localhost/api-produtos/product/read.php';
+        $curl = curl_init($url);
+
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER,true);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "GET");
+
+        $result = json_decode(curl_exec($curl));
+
+        var_dump($result);
     }
 }
 
